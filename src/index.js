a341fa4e6d0f9b01a3f0a0a445753372459f6b61
ogheneruro <ruronation2@yahoo.com>
2023-10-27 11:11:18 +0100

    react strict mode turned off

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**Use strict mode to carry out development only, turn it off for production build*/
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);*/
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(  
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
